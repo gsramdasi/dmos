2c8b83726c74387150870fc0e62346b27954ce3c
gsr-git <gramdasi@asu.edu>
2015-10-01 14:20:35 -0700

    Rarranged files. Put all files in respective folders

---
+++ project1/thread_test.c
@@ -0,0 +1,17 @@
+/*
+ * Main Test file
+ * 
+ * Members:
+ * Gourav
+ * Menaka
+ * Amaresh (1207301416)
+ *
+ */
+
+#include <stdio.h>
+#include "threads.h"
+
+void main(){
+	printf("Test\n");	
+}
+
